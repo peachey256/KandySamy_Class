92f380f25aaad015f2ca6eeb5f8ce3c677e8c5aa
Nathan L Schomer <nathan@nathanschomer.com>
2018-03-13 23:29:06 -0400

    added shared memory

--- Assignment4/source_solver/solver_kernel.cu
+++ Assignment4/source_solver/solver_kernel.cu
@@ -37,10 +37,57 @@ solver_kernel_naive(float *src, float *dest, double *diff)
 }
 
 __global__ void 
-solver_kernel_optimized()
+solver_kernel_optimized(float *src, float *dest, double *diff)
 {
-//copy src and dist to shared 
+    // create some shared memory
+    __shared__ float _src_shared[BLOCK_SIZE * BLOCK_SIZE];
+    __shared__ float _dst_shared[BLOCK_SIZE * BLOCK_SIZE];
 
+    // find yo self in global
+    int ty = blockDim.y * blockIdx.y + threadIdx.y;
+    int tx = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // find yo self in current block (for shared mem)
+    int threadLoc = threadIdx.y * blockDim.x + threadIdx.x;
+    int gridWidth = blockDim.x  * gridDim.x;
+
+    // subtract 2 from stride b/c of border
+    int stride = gridDim.x * blockDim.x - 2;
+
+    // stride stride stride
+    for( ; ty < GRID_DIMENSION - stride; ty += stride) {
+        for( ; tx < GRID_DIMENSION - stride; tx += stride) {
+           
+            // copy src and dest to shared
+            _src_shared[threadLoc] =  src[ty * gridWidth + tx];
+            _dst_shared[threadLoc] = dest[ty * gridWidth + tx];
+
+            // gotta wait for the slowpokes 
+            __syncthreads();
+            
+            // don't run if we're on the border
+            if ( tx && ty && tx<(blockDim.x) && ty<(blockDim.y) ) {
+                float tmp = _dst_shared[threadLoc];
+
+                _dst_shared[threadIdx.y * blockDim.x + threadIdx.x] = 
+                    0.2*(_src_shared[threadIdx.y     * blockDim.x + threadIdx.x]+
+                         _src_shared[(threadIdx.y+1) * blockDim.x + threadIdx.x]+
+                         _src_shared[(threadIdx.y-1) * blockDim.x + threadIdx.x]+
+                         _src_shared[threadIdx.y     * blockDim.x + (threadIdx.x+1)]+
+                         _src_shared[threadIdx.y     * blockDim.x + (threadIdx.x-1)]);
+
+                // calculate diff and add to total diff
+                //  ... diff stays global
+                // this is going to introduce some thread divergence
+                double newDiff = fabs(_dst_shared[threadLoc] - tmp);
+                atomicAdd(diff, newDiff);
+            }
+
+            // copy dst_shared back to dest
+            dest[ty * gridWidth + tx] = _dst_shared[threadLoc];
+        }
+    }
+ 
 //update the values 
 
 //calculate diff (think this may stay in global) 
